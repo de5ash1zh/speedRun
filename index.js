e473e0ec802129b36458a59c61337b2e629548cf
Devashish <de5ash1zh@gmail.com>
2025-03-15 18:37:22 +0530

    implemented cors

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 const app = express();
-const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hii there");
+});
+
+app.get("/piyush", (req, res) => {
+  res.send("Piyush page");
 });
 
 app.listen(port, () => {
